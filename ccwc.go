fa3c3be18af70a7199565d5d744c36db6d40e635
aneevel <aneevel15@gmail.com>
2025-08-02 22:14:43 -0500

    implement step one - bytes check

--- ccwc.go
+++ ccwc.go
@@ -3,13 +3,32 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
 
 	bytesPtr := flag.Bool("c", true, "Prints the byte count")
-
 	flag.Parse()
 
-	fmt.Println("bytesPtr:", *bytesPtr)
+	fileArg := os.Args[1:]
+	if len(fileArg) > 0 {
+		if *bytesPtr {
+			file, err := os.Open(strings.Join(fileArg, ""))
+			check(err)
+
+			fileData, err := file.Stat()
+			check(err)
+
+			fmt.Printf("%d %s\n", fileData.Size(), fileData.Name())
+		}
+	}
+
 }
